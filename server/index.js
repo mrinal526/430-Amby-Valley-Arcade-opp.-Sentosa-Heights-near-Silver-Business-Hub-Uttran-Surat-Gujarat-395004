2272d1c3d4b5402e90407e7157137f01f6dc8917
Mrinal Sawarn <76811531+mrinal526@users.noreply.github.com>
2024-07-02 14:50:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ const handle = require('./handlers');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use(cors());
+// app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
