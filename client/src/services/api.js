fde6dba1925df3001db8bb737c3bd2315c750006
Mrinal Sawarn <76811531+mrinal526@users.noreply.github.com>
2024-07-02 17:01:56 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const host = 'https://mern-vote-backend.vercel.app';
+const host = 'https://mern-vote-backend.vercel.app/api';
 
 export const setToken = token => {
   if (token) {
